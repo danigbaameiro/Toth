b4536168e36d416392b54673746a0edbf8ebd6ea
loo9o <miriamte@ucm.es>
2017-12-02 01:38:24 +0100

    Comments

--- files/conexion.sh
+++ files/conexion.sh
@@ -1,20 +1,29 @@
+#!/bin/bash
+
+#Busca las interfaces wifi disponibles y guarda la primera,
+# suponemos que puede ponerse en modo monitor
 interfaces(){
   service network-manager restart
   iwconfig | grep "802.11" | cut -d " " -f1 > interfaces_toth
   clear
   interface_out=`sed -n 1p interfaces_toth`
 }
+
+#Pone en modo monitor la interfaz y guarda el nuevo nombre de la interfaz en modo monitor
 monitorMode(){
   airmon-ng start $interface_out > monmode
   #mon_interface=`grep "monitor mode" monmode | cut -d " " -f7 | cut -d "]" -f2`
   mon_interface=`grep "monitor mode" monmode | cut -d " " -f9 | cut -d "]" -f2 | cut -d ")" -f1`
-
 }
+
+#Escanea las redes wifi disponibles durante 10 segundos y mata el proceso para poder continuar
 scan(){
   { airodump-ng $mon_interface -w capture --write-interval 10 -o csv 2>> output.txt; } &
   sleep 10
   pkill airodump-ng
 }
+
+#Elige el nodo más cercano, con mejor señal, cuanto más cerca más cercano a 0 el valor
 detect(){
   grep nodo capture*.csv | cut -d "," -f 5,14 > senial
   sed -i 's/ //g' senial #quitar espacios en blanco
@@ -25,9 +34,13 @@ detect(){
 
   best_node=`grep $best senial | cut -d "," -f 2` #nodo con mejor señal
 }
+
+#Realiza la conexión wifi con el nodo
 connect(){
   nmcli device wifi connect $best_node password Password_Wifi_Here
 }
+
+#Borrado de archivos
 clean(){
   rm *.csv
   rm monmode
